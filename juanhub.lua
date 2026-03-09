c5d06b2f14a1a77a3149e00292da3dd37f67da67
juanfri15 <juanfri0007@gmail.com>
2026-03-09 22:33:39 +0100

    Update juanhub.lua

--- juanhub.lua
+++ juanhub.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+cal Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "juanfrihub",
@@ -86,7 +86,7 @@ local Button = MainTab:CreateButton({
 local Button = MainTab:CreateButton({
    Name = "esp and aimbot",
    Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/dorpew/cynalez/refs/heads/main/main.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/juanfri15/juanhub/refs/heads/main/esp%20and%20aimbot"))()
    end,
 })
 
